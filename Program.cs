73367287491c6c5b639917e3f04423308fc8fe62
Maria Luiza <malualper@hotmail.com>
2020-06-01 23:18:16 -0300

    idadealuno

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace idadealuno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine("Minha data completa de nascimento:");
+            DateTime MariaLuiza = new DateTime (1970, 02, 02, 12, 20, 00);
+            string MariaLuizanas = MariaLuiza.ToLongDateString(); //dtl = data longa 
+            string DiaDaSemanaQueNasci = MariaLuiza.ToString("dddd");
+            Console.WriteLine($"Nascida em: {MariaLuiza} dia da semana: {DiaDaSemanaQueNasci}");
+            
+            DateTime bday = MariaLuiza.AddYears(18);
+            string bday18 = bday.ToLongDateString();
+            Console.WriteLine($"Meu aniversário de 18 anos foi em uma {bday18}");
+
+        }
+    }
+}
